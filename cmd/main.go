a9051c9e10a8f4a4449d48dccde07dd1aacb83b1
Kdu Santos <anjoazrael10@gmail.com>
2025-12-15 13:12:32 -0300

    ref(main):ajustes de logica e comando -Criado a função inputData para as flag -Removendo default de switch -Removendo texto helpme -Parando execução do programa quando der erro

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -11,44 +10,59 @@ import (
 	"github.com/dossantoscarlos/genpdf_cli/internal/model"
 )
 
+func insertData(input *string, options *string, pages *string) {
+	flag.StringVar(input, "input", "", "Arquivo PDF de entrada (obrigatório)")
+	flag.StringVar(options, "option", "", "1:Extract PDF 2:Merge PDF 3:Compress PDF 4:Split PDF")
+	flag.StringVar(pages, "pages", "", "Páginas a extrair (ex: 1,2,5)")
+	flag.Parse()
+}
+
 func main() {
 	var err error
+	var input string
+	var options string
+	var pages string
 
-	input := flag.String("input", "", "Arquivo PDF de entrada (obrigatório)")
-	options := flag.String("option", "", "1:Extract PDF 2:Merge PDF 3:Compress PDF 4:Split PDF")
-	pages := flag.String("pages", "", "Páginas a extrair (ex: 1,2,5)")
+	insertData(&input, &options, &pages)
 
-	flag.Parse()
-
-	if *input == "" || *pages == "" {
-		fmt.Println("Uso: go run main.go --input arquivo.pdf --option 1 --pages 1,2,3 or all para todas as páginas")
+	if input == "" || pages == "" {
 		flag.PrintDefaults()
+		log.Default().Fatalln("input ou pages vazio")
 		return
 	}
 
-	option, err := strconv.Atoi(*options)
+	log.Default().Println(input)
+	log.Default().Println(pages)
+	log.Default().Println(options)
+
+	option, err := strconv.Atoi(options)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		flag.PrintDefaults()
+		log.Fatalf("error option: %v", err.Error())
 		return
 	}
 
-	pageRanges := strings.Split(*pages, ",")
+	if option < 1 || option > 4 {
+		flag.PrintDefaults()
+		log.Fatalln("Opcao invalida")
+		return
+	}
+
+	pageRanges := strings.Split(pages, ",")
 
 	pdf := model.Pdf{
-		InputValueName: *input,
+		InputValueName: input,
 		PageRange:      pageRanges,
 	}
 
 	switch option {
 	case 1:
 		execapp.ExtractPDF(pdf)
-	case 2:
-		execapp.CompressPDF(pdf)
 	case 3:
+		execapp.CompressPDF(pdf)
+	case 2:
 		execapp.MergePDF(pdf)
 	case 4:
 		execapp.SplitPDF(pdf)
-	default:
-		log.Panicf("Error ao selecionar diretorio")
 	}
 }
